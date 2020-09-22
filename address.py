b294ab1d9744500cc99c56ff85e74c6021e40af3
Lyubov <Lyubov.Kovalevich@iitrust.ru>
2020-09-22 13:04:28 +0500

    Выделение вспомогательного класса address.py

---
+++ address.py
@@ -0,0 +1,7 @@
+class Address:
+    def __init__(self, firstname, lastname, address, phone, email):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.address = address
+        self.phone = phone
+        self.email = email
