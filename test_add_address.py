5a05a2f02fb651af9944fc7543086c4d8f6d8420
Lyubov <Lyubov.Kovalevich@iitrust.ru>
2020-09-22 12:53:50 +0500

    Выделение и передача параметров в методы

--- test_add_address.py
+++ test_add_address.py
@@ -15,33 +15,33 @@ class TestAddAddress(unittest.TestCase):
     def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def create_address(self, wd):
+    def create_address(self, wd, firstname, lastname, address, phone, email):
         # init address creation
         wd.find_element_by_link_text("add new").click()
         # fill address form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("Lev")
+        wd.find_element_by_name("firstname").send_keys(firstname)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("Tolstoy")
+        wd.find_element_by_name("lastname").send_keys(lastname)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("Russia")
+        wd.find_element_by_name("address").send_keys(address)
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("+79310002345")
+        wd.find_element_by_name("home").send_keys(phone)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("lev@mail.ru")
+        wd.find_element_by_name("email").send_keys(email)
         # submit address creation
         wd.find_element_by_name("submit").click()
 
@@ -54,8 +54,8 @@ class TestAddAddress(unittest.TestCase):
     def test_add_address(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
-        self.create_address(wd)
+        self.login(wd, username="admin", password="secret")
+        self.create_address(wd, firstname="Lev", lastname="Tolstoy", address="Russia", phone="+79310002345", email="lev@mail.ru")
         self.return_to_main_page(wd)
         self.logout(wd)
 
