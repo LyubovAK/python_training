a89268e4f9bcbc350546f811865748f170449c86
Lyubov <Lyubov.Kovalevich@iitrust.ru>
2020-09-22 12:40:08 +0500

    Выделение вспомогательных методов

--- test_add_address.py
+++ test_add_address.py
@@ -11,18 +11,19 @@ class TestAddAddress(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
-    
-    def test_add_address(self):
-        wd = self.wd
-        # open home page
+
+    def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
-        # login
+
+    def login(self, wd):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def create_address(self, wd):
         # init address creation
         wd.find_element_by_link_text("add new").click()
         # fill address form
@@ -43,11 +44,21 @@ class TestAddAddress(unittest.TestCase):
         wd.find_element_by_name("email").send_keys("lev@mail.ru")
         # submit address creation
         wd.find_element_by_name("submit").click()
-        # return to home page
+
+    def return_to_main_page(self, wd):
         wd.find_element_by_link_text("home").click()
-        # logout
+
+    def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
-    
+
+    def test_add_address(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd)
+        self.create_address(wd)
+        self.return_to_main_page(wd)
+        self.logout(wd)
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
