05445f896ebad374eab91e6bfeef6f063dd879f9
Lyubov <Lyubov.Kovalevich@iitrust.ru>
2020-09-25 15:53:18 +0500

    Выделены классы для работы с группами и контактами

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,5 +12,5 @@ def app(request):
 
 def test_add_address(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="Lev", lastname="Tolstoy", address="Russia", phone="+79310002345", email="lev@mail.ru"))
+    app.contact.create(Contact(firstname="Lev", lastname="Tolstoy", address="Russia", phone="+79310002345", email="lev@mail.ru"))
     app.session.logout()
