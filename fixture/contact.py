e84cdd953b2bb07f9b67b4d563ac4de15a46e3e3
Lyubov <Lyubov.Kovalevich@iitrust.ru>
2020-09-25 22:07:46 +0500

    Добавлен тест редактирования первого контакта test_edit_contact.py

--- fixture/contact.py
+++ fixture/contact.py
@@ -36,6 +36,30 @@ class ContactHelper:
         wd.switch_to_alert().accept()
         self.return_to_main_page()
 
+    def edit_first_contact(self,contact):
+        wd = self.app.wd
+        # open for editing
+        wd.find_element_by_xpath("//img[@alt='Edit']").click()
+        # edit contact
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.phone)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        # submit update
+        wd.find_element_by_name("update").click()
+        self.return_to_main_page()
+
     def return_to_main_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("home").click()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
-import pytest
 from model.contact import Contact
-from fixture.application import Application
 
 
-def test_add_address(app):
+def test_add_contact(app):
     app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="Lev", lastname="Tolstoy", address="Russia", phone="+79310002345", email="lev@mail.ru"))
     app.session.logout()
