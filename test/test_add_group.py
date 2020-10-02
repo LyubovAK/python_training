5ee235ebf2ffb857b04b35094700fd0e09262288
Lyubov <Lyubov.Kovalevich@iitrust.ru>
2020-10-02 13:24:32 +0500

    Добавлена проверка текущего состояния сессии

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -6,7 +6,6 @@ from model.group import Group
 def test_add_group(app):
     app.group.create(Group(name="group1", header="group11", footer="group111"))
 
-
 def test_add_empty_group(app):
     app.group.create(Group(name="", header="", footer=""))
 
