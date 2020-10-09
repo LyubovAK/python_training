1bc3c27707751bd728197727ff1d7f115013f50d
Lyubov <Lyubov.Kovalevich@iitrust.ru>
2020-10-09 17:57:00 +0500

    Доработен тест удаления контакта, удаляется случайно выбранный элемент

--- fixture/contact.py
+++ fixture/contact.py
@@ -38,10 +38,13 @@ class ContactHelper:
         self.contact_cache = None
 
     def delete_first_contact(self):
+        self.delete_contact_by_index(0)
+
+    def delete_contact_by_index(self, index):
         wd = self.app.wd
         self.open_home_page()
         # select first contact
-        wd.find_element_by_name("selected[]").click()
+        wd.find_elements_by_name("selected[]")[index].click()
         #submit deletion
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.switch_to_alert().accept()
