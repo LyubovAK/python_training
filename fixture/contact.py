727442cab0fd71850a2769c45261e8f78e847b9d
Lyubov <Lyubov.Kovalevich@iitrust.ru>
2020-10-16 17:36:41 +0500

    Добавлена параметризация при создании контакта, использование случайно сгенерированной строки

--- fixture/contact.py
+++ fixture/contact.py
@@ -89,8 +89,8 @@ class ContactHelper:
                 id = element.find_element_by_name("selected[]").get_attribute("value")
                 all_phones = element.find_element_by_xpath(".//td[6]").text
                 all_emails = element.find_element_by_xpath(".//td[5]").text
-                self.contact_cache.append(Contact(firstname = firstname, lastname = lastname, address= address,
-                                                  id = id, all_phones_from_home_page = all_phones, all_emails_from_home_page=all_emails))
+                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, address=address,
+                                                  id=id, all_phones_from_home_page=all_phones, all_emails_from_home_page=all_emails))
         return list(self.contact_cache)
 
     def open_contact_to_edit_by_index(self,index):
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,10 +1,22 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-def test_add_contact(app):
+testdata = [Contact(firstname="", lastname="", address="", homephone="", email="")] + [
+    Contact(firstname=random_string("firstname", 5), lastname=random_string("lastname", 10),
+            address=random_string("address", 20), homephone=(random_string("homephone", 10)), email=(random_string("email", 7)))
+    for i in range(5)
+]
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Lev", lastname="Tolstoy", address="Russia", homephone="+79310002345", email="lev@mail.ru")
     app.contact.create(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
