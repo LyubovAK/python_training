7aec085d4b0b8134224e7cdaffff6fee37534fec
Lyubov <Lyubov.Kovalevich@iitrust.ru>
2020-10-23 16:27:33 +0500

    Добавлена динамическая параметризация тестов

---
+++ data/contacts.py
@@ -0,0 +1,18 @@
+from model.contact import Contact
+# import random
+# import string
+
+
+testdata = [
+    Contact(firstname="Firstname1", lastname="Lastname2", address="Address1", homephone="123", email="123@mail.ru")
+]
+
+# def random_string(prefix, maxlen):
+#     symbols = string.ascii_letters + string.digits + " "*10
+#     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+#
+# testdata = [Contact(firstname="", lastname="", address="", homephone="", email="")] + [
+#     Contact(firstname=random_string("firstname", 5), lastname=random_string("lastname", 10),
+#             address=random_string("address", 20), homephone=(random_string("homephone", 10)), email=(random_string("email", 7)))
+#     for i in range(5)
+# ]
